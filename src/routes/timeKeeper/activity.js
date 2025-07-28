e9383c0e2f056a167af06bbd596033e46268015e
alimahmudlu <alimahmudlu@gmail.com>
2025-07-28 13:53:34 +0400

    update

--- src/routes/timeKeeper/activity.js
+++ src/routes/timeKeeper/activity.js
@@ -195,4 +195,55 @@ router.post('/reject', checkAuth, async (req, res) => {
     })
 })
 
+router.get('/checkin', checkAuth, async (req, res) => {
+    const {rows} = await db.query(`
+        SELECT ea.*, json_build_object(
+                'id', e.id,
+                'full_name', e.full_name,
+                'email', e.email,
+                'role', json_build_object(
+                        'id', er.id,
+                        'name', r.name
+                        )
+                     ) as employee FROM employee_activities ea
+                                            LEFT JOIN employees e ON e.id = ea.employee_id
+                                            LEFT JOIN employee_roles er ON e.id = er.employee_id
+                                            LEFT JOIN roles r ON r.id = er.role
+        WHERE ea.type = 1 AND ea.status > 0
+        ORDER BY ea.id DESC;
+        `)
+
+    res.status(200).json({
+        success: true,
+        message: 'Activity fetched successfully',
+        data: rows || []
+    })
+})
+
+router.get('/checkout', checkAuth, async (req, res) => {
+    const {rows} = await db.query(`
+        SELECT ea.*, json_build_object(
+                'id', e.id,
+                'full_name', e.full_name,
+                'email', e.email,
+                'role', json_build_object(
+                        'id', er.id,
+                        'name', r.name
+                        )
+                     ) as employee FROM employee_activities ea
+                                            LEFT JOIN employees e ON e.id = ea.employee_id
+                                            LEFT JOIN employee_roles er ON e.id = er.employee_id
+                                            LEFT JOIN roles r ON r.id = er.role
+        WHERE ea.type = 2 AND ea.status > 0
+        ORDER BY ea.id DESC;
+        `)
+
+    res.status(200).json({
+        success: true,
+        message: 'Activity fetched successfully',
+        data: rows || []
+    })
+})
+
+
 export default router
