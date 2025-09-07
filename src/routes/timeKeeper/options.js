8fcb4847b89ee3e4d0af3b2885bb15e9391fb6aa
alimahmudlu <alimahmudlu@gmail.com>
2025-09-07 08:31:50 +0400

    update

--- src/routes/timeKeeper/options.js
+++ src/routes/timeKeeper/options.js
@@ -8,32 +8,14 @@ const router = express.Router()
 router.get('/projects', checkAuth, async (req, res) => {
     const {rows} = await db.query(`SELECT
                                        p.*,
-                                       (SELECT json_build_object('id', r.id, 'name', r.name)  FROM roles r
-                                        WHERE role_id = pm.role_id LIMIT 1) as role_id,
-    (
-        SELECT COALESCE(
-                       jsonb_agg(
-                               jsonb_build_object(
-                                       'id', e1.id,
-                                       'full_name', e1.full_name,
-                                       'phone_number', e1.phone_number,
-                                       'role', jsonb_build_object(
-                                               'id', r.id,
-                                               'name', r.name
-                                               )
-                               )
-                       ),
-                       '[]'::jsonb
-               )
-        FROM project_members pm1
-                 JOIN employees e1 ON e1.id = pm1.employee_id
-                 LEFT JOIN employee_roles er ON er.employee_id = e1.id
-                 LEFT JOIN roles r ON r.id = er.role
-        WHERE pm1.project_id = p.id
-    ) AS members
+                                       (
+                                            SELECT json_build_object('id', r.id, 'name', r.name)  
+                                            FROM roles r
+                                            WHERE role_id = pm.role_id LIMIT 1
+                                       ) as role_id
                                    FROM project_members pm
-                                       LEFT JOIN projects p ON p.id = pm.project_id
-                                   WHERE pm.employee_id = $1`, [req.currentUserId])
+                                   JOIN projects p ON p.id = pm.project_id
+                                   WHERE pm.employee_id = $1  AND pm.status = 1`, [req.currentUserId])
 
     res.json({
         success: true,
