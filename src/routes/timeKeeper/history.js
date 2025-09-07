5823285ace4f6c963cd049abca966c553f356ac3
alimahmudlu <alimahmudlu@gmail.com>
2025-09-07 09:28:16 +0400

    update

--- src/routes/timeKeeper/history.js
+++ src/routes/timeKeeper/history.js
@@ -26,7 +26,8 @@ router.get('/list', checkAuth, async (req, res) => {
             SELECT 1
             FROM project_members pm1
                      JOIN project_members pm2 ON pm1.project_id = pm2.project_id
-            WHERE pm1.project_id = $${idx}
+            WHERE pm1.employee_id = ea.employee_id
+            AND pm1.project_id = $${idx}
         )`);
         values.push(project)
         idx++
@@ -61,32 +62,6 @@ router.get('/list', checkAuth, async (req, res) => {
                                    ${filters.length > 0 ? ` AND ${filters.join(' AND ')}` : ''}
         ORDER BY ea.id DESC;
         `, [req.currentUserId, ...values])
-
-    console.log(`
-        SELECT ea.*, json_build_object(
-                'id', e.id,
-                'full_name', e.full_name,
-                'email', e.email,
-                'manual', e.dont_have_phone,
-                'role', json_build_object(
-                        'id', er.id,
-                        'name', r.name
-                        )
-                     ) as employee FROM employee_activities ea
-                                            LEFT JOIN employees e ON e.id = ea.employee_id
-                                            LEFT JOIN employee_roles er ON e.id = er.employee_id
-                                            LEFT JOIN roles r ON r.id = er.role
-
-        WHERE EXISTS (
-            SELECT 1
-            FROM project_members pm1
-                     JOIN project_members pm2 ON pm1.project_id = pm2.project_id
-            WHERE pm1.employee_id = ea.employee_id
-              AND pm2.employee_id = $1
-        )
-            ${filters.length > 0 ? ` AND ${filters.join(' AND ')}` : ''}
-        ORDER BY ea.id DESC;
-`, [req.currentUserId, ...values], req.query, req.query?.project, req.query?.project?.id)
 
     res.status(200).json({
         success: true,
