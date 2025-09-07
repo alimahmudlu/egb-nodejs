ad81d9ad2d2554b3cbd24fb39a55a85d81f1f9e8
alimahmudlu <alimahmudlu@gmail.com>
2025-09-07 08:36:00 +0400

    update

--- src/routes/timeKeeper/options.js
+++ src/routes/timeKeeper/options.js
@@ -12,7 +12,28 @@ router.get('/projects', checkAuth, async (req, res) => {
                                             SELECT json_build_object('id', r.id, 'name', r.name)  
                                             FROM roles r
                                             WHERE role_id = pm.role_id LIMIT 1
-                                       ) as role_id
+                                       ) as role_id,
+                                        (
+                                            SELECT COALESCE(
+                                                jsonb_agg(
+                                                    jsonb_build_object(
+                                                        'id', e1.id,
+                                                        'full_name', e1.full_name,
+                                                        'phone_number', e1.phone_number,
+                                                        'role', jsonb_build_object(
+                                                            'id', r.id,
+                                                            'name', r.name
+                                                        )
+                                                    )
+                                                ),
+                                                '[]'::jsonb
+                                            )
+                                            FROM project_members pm1
+                                            JOIN employees e1 ON e1.id = pm1.employee_id
+                                            LEFT JOIN employee_roles er ON er.employee_id = e1.id
+                                            LEFT JOIN roles r ON r.id = er.role
+                                            WHERE pm1.project_id = p.id
+                                        ) AS members
                                    FROM project_members pm
                                    JOIN projects p ON p.id = pm.project_id
                                    WHERE pm.employee_id = $1  AND pm.status = 1`, [req.currentUserId])
