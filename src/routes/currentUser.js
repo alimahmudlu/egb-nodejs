1d1d5778a59bd4cdd704310b0d77a3f826eb2f0c
alimahmudlu <alimahmudlu@gmail.com>
2025-09-14 05:27:03 +0400

    update

--- src/routes/currentUser.js
+++ src/routes/currentUser.js
@@ -177,9 +177,9 @@ router.get('/activities/work_hours', checkAuth, async (req, res) => {
                     ELSE 'Completed'
                     END                     AS activity_status,
                 CASE
-                    WHEN exit.status = 3 THEN 3
-                    ELSE 4
-                    END                     AS activity_status_id
+                    WHEN exit.status = 3 THEN 'activityRejectedExit'
+                    ELSE 'activityCompleted'
+                    END                     AS activity_status_key
             FROM employee_activities entry
                      JOIN employee_activities exit
                           ON entry.employee_id = exit.employee_id
@@ -219,7 +219,7 @@ router.get('/activities/work_hours', checkAuth, async (req, res) => {
                 NULL                    AS exit_longitude,
                 NULL                    AS work_duration,
                 'No Exit'               AS activity_status,
-                2                       AS activity_status_id
+                'activityNoExit'        AS activity_status_key
             FROM employee_activities entry
             WHERE entry.type = 1
               AND entry.status = 2
@@ -253,7 +253,7 @@ router.get('/activities/work_hours', checkAuth, async (req, res) => {
                 NULL                    AS exit_longitude,
                 NULL                    AS work_duration,
                 'Rejected Entry'        AS activity_status,
-                1                       AS activity_status_id
+                'activityRejectedEntry' AS activity_status_id
             FROM employee_activities entry
             WHERE entry.type = 1
               AND entry.status = 3
