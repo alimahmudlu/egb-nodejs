3c0220f3b87d8d27e13f383213204f7bea2606fa
alimahmudlu <alimahmudlu@gmail.com>
2025-09-25 11:08:37 +0400

    update

--- src/routes/timeKeeper/overtime.js
+++ src/routes/timeKeeper/overtime.js
@@ -141,8 +141,6 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
     const io = getIO();
     const socketId = userSocketMap.get(employee_id);
 
-    console.log('socket_id', socketId, returnedRow?.[0])
-
     if (socketId) {
         io.to(socketId).emit("update_activity", {
             success: true,
