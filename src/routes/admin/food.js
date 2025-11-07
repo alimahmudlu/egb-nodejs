6f092167a5fc3f09c22f5da3f6b6cd9498f7c067
alimahmudlu <alimahmudlu@gmail.com>
2025-11-07 14:49:38 +0400

    update

--- src/routes/admin/food.js
+++ src/routes/admin/food.js
@@ -7,6 +7,22 @@ import userPermission from "../../middleware/userPermission.js";
 
 const router = express.Router()
 
+// router.post('/report/daily', checkAuth, userPermission, async (req, res) => {
+//     const {date} = req.params;
+//
+//     const {rows} = await db.query(`
+//         INSERT INTO food_reports (turn1order, turn2order, date, employee_id)
+//         VALUES ($1, $2, $3, $4)
+//         RETURNING *
+//     `, [turn1order, turn2order, date, req.currentUserId]);
+//
+//     return res.status(200).json({
+//         success: true,
+//         message: 'Food report added successfully',
+//         data: rows?.[0]
+//     })
+// })
+
 router.post('/report/add', checkAuth, userPermission, async (req, res) => {
     const {turn1order, turn2order, date} = req.body;
     // req.currentUserId
@@ -20,6 +36,20 @@ router.post('/report/add', checkAuth, userPermission, async (req, res) => {
     return res.status(200).json({
         success: true,
         message: 'Food report added successfully',
+        data: rows?.[0]
+    })
+})
+
+router.get('/report/list', checkAuth, userPermission, async (req, res) => {
+    const {rows} = await db.query(`
+        SELECT *
+        FROM food_reports
+        WHERE employee_id = $1
+    `, [req.currentUserId]);
+
+    return res.status(200).json({
+        success: true,
+        message: 'Food reports fetched successfully',
         data: rows?.[0]
     })
 })
