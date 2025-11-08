894e133188e2f9ff9a9ed9bf4476edef9a088d9b
alimahmudlu <alimahmudlu@gmail.com>
2025-11-08 05:14:38 +0400

    update

--- src/routes/admin/food.js
+++ src/routes/admin/food.js
@@ -4,6 +4,7 @@ import checkAuth from '../../middleware/checkAuth.js'
 import {getIO, userSocketMap} from "../../socketManager.js";
 import sendPushNotification from "../../helper/sendPushNotification.js";
 import userPermission from "../../middleware/userPermission.js";
+import moment from "moment";
 
 const router = express.Router()
 
@@ -60,6 +61,22 @@ router.get('/report/list', checkAuth, userPermission, async (req, res) => {
         FROM food_reports
         WHERE employee_id = $1
     `, [req.currentUserId]);
+
+    return res.status(200).json({
+        success: true,
+        message: 'Food reports fetched successfully',
+        data: rows
+    })
+})
+
+router.get('/report/today', checkAuth, userPermission, async (req, res) => {
+    const {rows} = await db.query(`
+        SELECT fr.*,
+               (SELECT COUNT(ea.id) FROM employee_activities ea WHERE ea.status = 2 AND ea.completed_status = 1 AND DATE(ea.review_time) = $1 ) AS turn1employees,
+               (SELECT COUNT(ea.id) FROM employee_activities ea WHERE ea.status = 2 AND ea.completed_status = 1 AND DATE(ea.review_time) = $1 ) AS turn2employees,
+        FROM food_reports fr
+        WHERE date = $2
+    `, [moment().add(-1, 'days').format('YYYY-MM-DD'), moment().add(1, 'days').format('YYYY-MM-DD')]);
 
     return res.status(200).json({
         success: true,
