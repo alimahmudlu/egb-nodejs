9527ee504e6afd2068a1f57955ebbf13031e7fb3
alimahmudlu <alimahmudlu@gmail.com>
2025-11-19 20:18:41 +0400

    update

--- src/routes/timeKeeper/activity.js
+++ src/routes/timeKeeper/activity.js
@@ -12,6 +12,72 @@ import timeKeeperActivityAccept from "../../fn/timeKeeperActivityAccept.js";
 const router = express.Router()
 
 router.get('/list', checkAuth, userPermission, async (req, res) => {
+    const {start_date, end_date, full_name, project, page, limit} = req.query;
+    const filters = [];
+    const values = [];
+    let idx = 2;
+
+    if (project) {
+        filters.push(`EXISTS (
+            SELECT 1
+            FROM project_members pm1
+                     JOIN project_members pm2 ON pm1.project_id = pm2.project_id
+            WHERE pm1.employee_id = ea.employee_id
+            AND pm1.project_id = $${idx}
+        )`);
+        values.push(project)
+        idx++
+    }
+    if (full_name) {
+        filters.push(`(LOWER(e.full_name) LIKE LOWER($${idx}))`);
+        values.push(`%${full_name}%`);
+        idx++
+    }
+
+    console.log((`
+        SELECT ea.*, json_build_object(
+                'id', e.id,
+                'full_name', e.full_name,
+                'email', e.email,
+                'role', json_build_object(
+                        'id', er.id,
+                        'name', r.name
+                        )
+                     ) as employee,
+               (
+                   SELECT json_build_object(
+                                  'id', e.id,
+                                  'full_name', e.full_name
+                          )
+                   FROM employees e
+                   WHERE e.id = ea.reviewer_employee_id
+                          LIMIT 1
+            ) AS reviewer,
+               (
+                   SELECT json_build_object(
+                                  'id', p.id,
+                                  'name', p.name
+                          )
+                   FROM project_members pm
+                   LEFT JOIN projects p ON p.id = pm.project_id
+                   WHERE e.id = pm.employee_id AND pm.status = 1
+                          LIMIT 1
+            ) AS project
+        FROM employee_activities ea
+            LEFT JOIN employees e ON e.id = ea.employee_id
+            LEFT JOIN employee_roles er ON e.id = er.employee_id
+            LEFT JOIN roles r ON r.id = er.role
+        WHERE EXISTS (
+            SELECT 1
+            FROM project_members pm1
+            JOIN project_members pm2 ON pm1.project_id = pm2.project_id AND pm2.status = 1
+            WHERE pm1.employee_id = ea.employee_id AND pm1.status = 1
+          AND pm2.employee_id = $1
+            )
+            ${filters.length > 0 ? ` AND ${filters.join(' AND ')}` : ''}
+        ORDER BY e.full_name ASC;
+    `, [req.currentUserId, ...values]))
+
     const {rows} = await db.query(`
         SELECT ea.*, json_build_object(
                 'id', e.id,
@@ -52,8 +118,9 @@ router.get('/list', checkAuth, userPermission, async (req, res) => {
             WHERE pm1.employee_id = ea.employee_id AND pm1.status = 1
           AND pm2.employee_id = $1
             )
+            ${filters.length > 0 ? ` AND ${filters.join(' AND ')}` : ''}
         ORDER BY e.full_name ASC;
-    `, [req.currentUserId])
+    `, [req.currentUserId, ...values])
 
     res.status(200).json({
         success: true,
