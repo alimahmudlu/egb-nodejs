9e392743ae7ba1b4d84832e767ead6284037d058
alimahmudlu <alimahmudlu@gmail.com>
2025-11-22 03:59:50 +0400

    update

--- src/routes/admin/bus.js
+++ src/routes/admin/bus.js
@@ -13,21 +13,20 @@ router.get('/projects', checkAuth, userPermission, async (req, res) => {
         SELECT
             p.name AS project_name,
             p.id AS project_id,
+            moment().format('YYYY-MM-DD') as date,
             COUNT(CASE WHEN ea.turn = 1 THEN 1 END) AS turn1Employees,
-            COUNT(CASE WHEN ea.turn = 2 THEN 1 END) AS turn2Employees
-        FROM
-            projects AS p
-                JOIN
-            project_members AS pm ON p.id = pm.project_id
-                AND pm.status = 1 -- Layihə üzvü aktivdir
-                JOIN
-            employees AS e ON e.id = pm.employee_id
-                JOIN
-            employee_activities AS ea ON ea.employee_id = e.id
-                AND ea.status = 2 -- Fəaliyyət statusu 2
-                AND ea.completed_status = 0 -- Tamamlanma statusu 1
-                AND ea.type = 1 -- Fəaliyyət növü 1 (Check-in/out olduğu güman edilir)
-                AND DATE(ea.review_time) = $1 -- Tələb olunan tarix
+            COUNT(CASE WHEN ea.turn = 2 THEN 1 END) AS turn2Employees,
+            (SELECT br.id FROM bus_reports br WHERE br.project_id = p.id AND Date(br.date) = $1 ORDER BY br.id DESC LIMIT 1) AS report_status
+        FROM projects AS p
+        JOIN project_members AS pm ON p.id = pm.project_id 
+            AND pm.status = 1 -- Layihə üzvü aktivdir
+        JOIN employees AS e ON e.id = pm.employee_id
+        JOIN employee_activities AS ea ON ea.employee_id = e.id
+            AND ea.status = 2 -- Fəaliyyət statusu 2
+            AND ea.completed_status = 0 -- Tamamlanma statusu 1
+            AND ea.type = 1 -- Fəaliyyət növü 1 (Check-in/out olduğu güman edilir)
+            AND DATE(ea.review_time) = $1 -- Tələb olunan tarix
+        
         GROUP BY
             p.id, p.name
         ORDER BY
