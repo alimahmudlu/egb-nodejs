518b9db325ee53c6571cb94ee5110fe9bdd50012
alimahmudlu <alimahmudlu@gmail.com>
2025-11-28 15:22:28 +0400

    update

--- src/helper/sendPushNotification.js
+++ src/helper/sendPushNotification.js
@@ -11,19 +11,12 @@ const chunkArray = (array, size) => {
 };
 
 export default async function sendPushNotification(userId, title, body, data = {}) {
-    // 1. Aktiv Tokenləri Bazadan Götür
     const { rows } = await db.query(`
         SELECT token FROM notification_tokens
         WHERE user_id = $1 AND status = 1
     `, [userId]);
 
-    console.log(rows, userId, 'rows', `
-        SELECT token FROM notification_tokens
-        WHERE user_id = $1 AND status = 1
-    `)
-
     if (rows.length === 0) {
-        console.log(`[PushService] İstifadəçi ${userId} üçün aktiv token tapılmadı.`);
         return;
     }
 
@@ -55,72 +48,8 @@ export default async function sendPushNotification(userId, title, body, data = {
 
         const result = await res.json();
 
-        console.log(result, 'result')
-
         if (result?.data?.status === 'error') {
             const {rows: deletedRow} = db.query(`UPDATE notification_tokens SET status = 0 WHERE token = $1`, [row?.token])
         }
     })
-
-    // const tokens = rows.map(row => row.token);
-    // const tokenBatches = chunkArray(tokens, 100);
-    //
-    // const failedTokens = [];
-    //
-    // for (const batch of rows) {
-    //     const messages = batch.map(token => ({
-    //         to: token,
-    //         sound: 'default',
-    //         title,
-    //         body,
-    //         subtitle: "Yeni Məlumat",
-    //         priority: 'high',
-    //         channelId: 'default',
-    //         data: {
-    //             event: 'new_data',
-    //             timestamp: Date.now(),
-    //             ...data
-    //         },
-    //     }));
-    //
-    //     try {
-    //         const res = await fetch("https://exp.host/--/api/v2/push/send", {
-    //             method: "POST",
-    //             headers: {
-    //                 Accept: "application/json",
-    //                 "Accept-encoding": "gzip, deflate",
-    //                 "Content-Type": "application/json",
-    //             },
-    //             body: JSON.stringify(messages),
-    //         });
-    //
-    //         const result = await res.json();
-    //
-    //         if (result?.data) {
-    //             for (const ticket of result.data) {
-    //                 if (ticket.status === 'error' && ticket.details?.error === 'DeviceNotRegistered') {
-    //                     const token = messages.find(m => m.id === ticket.id)?.to || null;
-    //                     const index = result.data.indexOf(ticket);
-    //                     if (index !== -1 && batch[index]) {
-    //                         failedTokens.push(batch[index]);
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //
-    //     } catch (error) {
-    //         console.error("[PushService] Expo API ilə əlaqə xətası:", error);
-    //     }
-    // }
-    //
-    // if (failedTokens.length > 0) {
-    //     const tokenList = failedTokens.join("','");
-    //
-    //     await db.query(`
-    //         UPDATE notification_tokens
-    //         SET status = 0
-    //         WHERE token IN ('${tokenList}')
-    //     `);
-    //     console.log(`[PushService] ${failedTokens.length} qeyri-aktiv token deaktiv edildi.`);
-    // }
 }
