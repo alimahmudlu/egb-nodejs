938b1bc00927b329c8c01d1a576d709edbf0a9ea
alimahmudlu <alimahmudlu@gmail.com>
2025-12-02 04:07:08 +0400

    update

--- src/routes/timeKeeper/activity.js
+++ src/routes/timeKeeper/activity.js
@@ -520,8 +520,6 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
         minutes: 0
     }
 
-    console.log(checkInControlRow?.[0]?.review_time, type , 'aslakslaskla')
-
     if (checkInControlRow?.[0]?.review_time && type === 2) {
         const start = moment(checkInControlRow?.[0].review_time, 'YYYY-MM-DD HH:mm');
         const end = moment(confirm_time, 'YYYY-MM-DD HH:mm').endOf('minute');
@@ -543,7 +541,6 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
             confirm_type === 1 &&
             moment().tz("Europe/Moscow").weekday() !== 2
         ) {
-            console.log('1ci ')
             diff = {
                 hours: 10,
                 minutes: 0
@@ -556,7 +553,6 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
             confirm_type === 4 &&
             moment().tz("Europe/Moscow").weekday() === 2
         ) {
-            console.log('2ci ')
             diff = {
                 hours: 10,
                 minutes: 0
@@ -569,7 +565,6 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
             confirm_type === 1 &&
             moment().tz("Europe/Moscow").weekday() === 2
         ) {
-            console.log('3ci ')
             diff = {
                 hours: 8,
                 minutes: 0
@@ -577,11 +572,10 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
         }
         else if (
             // moment(startHourMinute, "HH:mm").isBetween(moment("07:29", "HH:mm"), moment("08:31", "HH:mm")) &&
-            // duration.asHours() < 24 &&
+            duration.asHours() < 24 &&
             confirm_type === 1 &&
             moment().tz("Europe/Moscow").weekday() !== 2
         ) {
-            console.log('4ci ')
             diff = {
                 hours: 10,
                 minutes: 0
@@ -589,11 +583,10 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
         }
         else if (
             // moment(startHourMinute, "HH:mm").isBetween(moment("07:29", "HH:mm"), moment("08:31", "HH:mm")) &&
-            // duration.asHours() < 24 &&
+            duration.asHours() < 24 &&
             confirm_type === 4 &&
             moment().tz("Europe/Moscow").weekday() === 2
         ) {
-            console.log('5ci ')
             diff = {
                 hours: 10,
                 minutes: 0
@@ -601,11 +594,10 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
         }
         else if (
             // moment(startHourMinute, "HH:mm").isBetween(moment("07:29", "HH:mm"), moment("08:31", "HH:mm")) &&
-            // duration.asHours() < 24 &&
+            duration.asHours() < 24 &&
             confirm_type === 1 &&
             moment().tz("Europe/Moscow").weekday() === 2
         ) {
-            console.log('6ci ')
             diff = {
                 hours: 8,
                 minutes: 0
@@ -617,7 +609,6 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
             duration.asHours() < 24 &&
             confirm_type === 2
         ) {
-            console.log('7ci ')
             diff = {
                 hours: 5,
                 minutes: 0
@@ -629,7 +620,6 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
             duration.asHours() < 24 &&
             confirm_type === 2
         ) {
-            console.log('8ci ')
             diff = {
                 hours: Math.floor(duration.asHours() - 1),
                 minutes: duration.minutes()
@@ -662,7 +652,6 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
             confirm_type === 1 &&
             moment().tz("Europe/Moscow").weekday() !== 2
         ) {
-            console.log('9ci ')
             diff = {
                 hours: 10,
                 minutes: 0
@@ -675,7 +664,6 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
             confirm_type === 1 &&
             moment().tz("Europe/Moscow").weekday() === 2
         ) {
-            console.log('10ci ')
             diff = {
                 hours: 8,
                 minutes: 0
@@ -688,7 +676,6 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
             confirm_type === 4 &&
             moment().tz("Europe/Moscow").weekday() === 2
         ) {
-            console.log('11ci ')
             diff = {
                 hours: 10,
                 minutes: 0
@@ -700,7 +687,6 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
             duration.asHours() < 24 &&
             confirm_type === 2
         ) {
-            console.log('12ci ')
             diff = {
                 hours: 5,
                 minutes: 0
@@ -712,7 +698,6 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
             duration.asHours() < 24 &&
             confirm_type === 2
         ) {
-            console.log('13ci ')
             diff = {
                 hours: Math.floor(duration.asHours() - 1),
                 minutes: duration.minutes()
@@ -721,7 +706,6 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
         else if (
             confirm_type === 3
         ) {
-            console.log('14ci ')
             diff = {
                 hours: 0,
                 minutes: 0
@@ -742,68 +726,66 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
     //     }
     // }
 
-    console.log(diff, 'diff', confirm_type, moment().tz("Europe/Moscow").weekday(), moment().tz("Europe/Moscow").weekday() === 2)
+    const {rows: checkInRow} = await db.query(`
+        UPDATE employee_activities ea
+        SET completed_status = $1, work_time = $6
+        WHERE employee_id = $2 and status = $3 and completed_status = $4 and type = $5
+            RETURNING *;
+    `, [1, employee_id, 2, 0, 1, `${diff?.hours}:${diff?.minutes}`])
 
-    // const {rows: checkInRow} = await db.query(`
-    //     UPDATE employee_activities ea
-    //     SET completed_status = $1, work_time = $6
-    //     WHERE employee_id = $2 and status = $3 and completed_status = $4 and type = $5
-    //         RETURNING *;
-    // `, [1, employee_id, 2, 0, 1, `${diff?.hours}:${diff?.minutes}`])
-    //
-    // const {rows} = await db.query(`
-    //     UPDATE employee_activities
-    //     SET reviewer_employee_id = $1, reviewer_timezone = $2, review_time = $3, completed_status = $4, status = $9, confirm_type = $10
-    //     WHERE id = $5 and employee_id = $6 and status = $7 and type = $8
-    //         RETURNING *;
-    // `, [req.currentUserId, timezone, confirm_time, type === 1 ? 0 : 1, activity_id, employee_id, 1,  type, 2, confirm_type])
-    //
-    //
-    // if (rows.length === 0 && (type === 2 ? checkInRow.length === 0 : false)) {
-    //     return res.status(404).json({
-    //         success: false,
-    //         message: 'Activity not found or already accepted'
-    //     })
-    // }
-    //
-    // const {rows: returnedRow} = await db.query(
-    //     `SELECT ea.*,
-    //             (
-    //                 SELECT json_build_object(
-    //                                'id', e.id,
-    //                                'full_name', e.full_name
-    //                        )
-    //                 FROM employees e
-    //                 WHERE e.id = ea.reviewer_employee_id
-    //                 LIMIT 1
-    //          ) AS reviewer
-    //      FROM employee_activities ea
-    //      WHERE ea.id = $1`, [rows?.[0]?.id]
-    // )
-    //
-    //
-    // const io = getIO();
-    // const socketId = userSocketMap.get(employee_id);
-    //
-    // if (socketId) {
-    //     io.to(socketId).emit("update_activity", {
-    //         success: true,
-    //         from: req.currentUserId,
-    //         message: 'Activity status changed successfully',
-    //         data: returnedRow?.[0]
-    //     });
-    // }
-    //
-    // sendPushNotification(employee_id, type === 1 ? 'Check-in request accepted' : 'Check-out request accepted', `${empData?.[0]?.full_name} accepted a request for ${type === 1 ? 'check-in' : 'check-out'} at now`, {
-    //     url: '/timeKeeper/',
-    //     utm_source: 'push_notification'
-    // })
-    //
-    // return res.status(200).json({
-    //     success: true,
-    //     message: 'Activity accepted successfully',
-    //     data: returnedRow?.[0]
-    // })
+    const {rows} = await db.query(`
+        UPDATE employee_activities
+        SET reviewer_employee_id = $1, reviewer_timezone = $2, review_time = $3, completed_status = $4, status = $9, confirm_type = $10
+        WHERE id = $5 and employee_id = $6 and status = $7 and type = $8
+            RETURNING *;
+    `, [req.currentUserId, timezone, confirm_time, type === 1 ? 0 : 1, activity_id, employee_id, 1,  type, 2, confirm_type])
+
+
+    if (rows.length === 0 && (type === 2 ? checkInRow.length === 0 : false)) {
+        return res.status(404).json({
+            success: false,
+            message: 'Activity not found or already accepted'
+        })
+    }
+
+    const {rows: returnedRow} = await db.query(
+        `SELECT ea.*,
+                (
+                    SELECT json_build_object(
+                                   'id', e.id,
+                                   'full_name', e.full_name
+                           )
+                    FROM employees e
+                    WHERE e.id = ea.reviewer_employee_id
+                    LIMIT 1
+             ) AS reviewer
+         FROM employee_activities ea
+         WHERE ea.id = $1`, [rows?.[0]?.id]
+    )
+
+
+    const io = getIO();
+    const socketId = userSocketMap.get(employee_id);
+
+    if (socketId) {
+        io.to(socketId).emit("update_activity", {
+            success: true,
+            from: req.currentUserId,
+            message: 'Activity status changed successfully',
+            data: returnedRow?.[0]
+        });
+    }
+
+    sendPushNotification(employee_id, type === 1 ? 'Check-in request accepted' : 'Check-out request accepted', `${empData?.[0]?.full_name} accepted a request for ${type === 1 ? 'check-in' : 'check-out'} at now`, {
+        url: '/timeKeeper/',
+        utm_source: 'push_notification'
+    })
+
+    return res.status(200).json({
+        success: true,
+        message: 'Activity accepted successfully',
+        data: returnedRow?.[0]
+    })
 })
 
 router.post('/reject', checkAuth, userPermission, async (req, res) => {
@@ -1342,7 +1324,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             confirm_type === 1 &&
             moment().tz("Europe/Moscow").weekday() !== 2
         ) {
-            console.log('1ci ')
             diff = {
                 hours: 10,
                 minutes: 0
@@ -1355,7 +1336,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             confirm_type === 1 &&
             moment().tz("Europe/Moscow").weekday() === 2
         ) {
-            console.log('2ci ')
             diff = {
                 hours: 8,
                 minutes: 0
@@ -1368,7 +1348,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             confirm_type === 4 &&
             moment().tz("Europe/Moscow").weekday() === 2
         ) {
-            console.log('3cu ')
             diff = {
                 hours: 10,
                 minutes: 0
@@ -1380,7 +1359,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             confirm_type === 1 &&
             moment().tz("Europe/Moscow").weekday() !== 2
         ) {
-            console.log('4cu ')
             diff = {
                 hours: 10,
                 minutes: 0
@@ -1392,7 +1370,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             confirm_type === 1 &&
             moment().tz("Europe/Moscow").weekday() === 2
         ) {
-            console.log('5ci ')
             diff = {
                 hours: 8,
                 minutes: 0
@@ -1404,7 +1381,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             confirm_type === 4 &&
             moment().tz("Europe/Moscow").weekday() === 2
         ) {
-            console.log('6ci ')
             diff = {
                 hours: 10,
                 minutes: 0
@@ -1416,7 +1392,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             duration.asHours() < 24 &&
             confirm_type === 2
         ) {
-            console.log('7ci ')
             diff = {
                 hours: 5,
                 minutes: 0
@@ -1428,7 +1403,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             duration.asHours() < 24 &&
             confirm_type === 2
         ) {
-            console.log('8ci ')
             diff = {
                 hours: Math.floor(duration.asHours() - 1),
                 minutes: duration.minutes()
@@ -1461,7 +1435,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             confirm_type === 1 &&
             moment().tz("Europe/Moscow").weekday() !== 2
         ) {
-            console.log('9cu ')
             diff = {
                 hours: 10,
                 minutes: 0
@@ -1474,7 +1447,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             confirm_type === 1 &&
             moment().tz("Europe/Moscow").weekday() === 2
         ) {
-            console.log('10cu ')
             diff = {
                 hours: 8,
                 minutes: 0
@@ -1487,7 +1459,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             confirm_type === 4 &&
             moment().tz("Europe/Moscow").weekday() === 2
         ) {
-            console.log('11ci ')
             diff = {
                 hours: 10,
                 minutes: 0
@@ -1499,7 +1470,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             duration.asHours() < 24 &&
             confirm_type === 2
         ) {
-            console.log('12ci ')
             diff = {
                 hours: 5,
                 minutes: 0
@@ -1511,7 +1481,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             duration.asHours() < 24 &&
             confirm_type === 2
         ) {
-            console.log('13cu ')
             diff = {
                 hours: Math.floor(duration.asHours() - 1),
                 minutes: duration.minutes()
@@ -1520,7 +1489,6 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
         else if (
             confirm_type === 3
         ) {
-            console.log('14cu ')
             diff = {
                 hours: 0,
                 minutes: 0
@@ -1569,9 +1537,7 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
         // }
     }
 
-    console.log(diff, 'diff', confirm_type, moment().tz("Europe/Moscow").weekday(), moment().tz("Europe/Moscow").weekday() === 2)
-
- /*   const {rows: checkInRow} = await db.query(`
+    const {rows: checkInRow} = await db.query(`
         UPDATE employee_activities ea
         SET completed_status = $1, work_time = $6
         WHERE employee_id = $2 and status = $3 and completed_status = $4 and type = $5
@@ -1691,7 +1657,7 @@ router.post('/checkout', checkAuth, userPermission, async (req, res) => {
             message: 'activity already exists for this status',
             data: null
         })
-    }*/
+    }
 })
 
 router.get('/', checkAuth, userPermission, async (req, res) => {
