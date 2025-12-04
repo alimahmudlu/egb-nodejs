d75d9a93cc280e9f8a98ec60191412e1f96237fd
alimahmudlu <alimahmudlu@gmail.com>
2025-12-04 04:07:38 +0400

    update

--- src/routes/admin/food.js
+++ src/routes/admin/food.js
@@ -92,4 +92,38 @@ router.get('/report/today', checkAuth, userPermission, async (req, res) => {
     })
 })
 
+router.get('/projects', checkAuth, userPermission, async (req, res) => {
+    const query = `
+        SELECT
+            p.name AS project_name,
+            p.id AS project_id,
+            '${moment().format('YYYY-MM-DD')}' as date,
+            COUNT(CASE WHEN ea.turn = 1 THEN 1 END) AS turn1Employees,
+            COUNT(CASE WHEN ea.turn = 2 THEN 1 END) AS turn2Employees,
+            (SELECT to_jsonb(br.*) FROM bus_reports br WHERE br.project_id = p.id AND Date(br.date) = $1 ORDER BY br.id DESC LIMIT 1) AS report_status
+        FROM projects AS p
+            LEFT JOIN project_members AS pm ON p.id = pm.project_id
+            AND pm.status = 1 
+            LEFT JOIN employees AS e ON e.id = pm.employee_id
+            LEFT JOIN employee_activities AS ea ON ea.employee_id = e.id
+            AND ea.status = 2
+            AND ea.completed_status = 1
+            AND ea.type = 1
+            AND DATE(ea.review_time) = $1
+
+        GROUP BY
+            p.id, p.name
+        ORDER BY
+            p.id;
+    `
+
+    const {rows: employees} = await db.query(query, [moment().add(-1, 'days').format('YYYY-MM-DD')]);
+
+    return res.status(200).json({
+        success: true,
+        message: 'Food reports fetched successfully',
+        data: employees
+    })
+})
+
 export default router
