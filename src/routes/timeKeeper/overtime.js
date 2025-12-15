3e19061fe628e039f7daa9d6d4c20b38aca233a2
alimahmudlu <alimahmudlu@gmail.com>
2025-12-15 09:06:18 +0400

    update

--- src/routes/timeKeeper/overtime.js
+++ src/routes/timeKeeper/overtime.js
@@ -21,15 +21,6 @@ router.get('/list', checkAuth, userPermission, async (req, res) => {
                         'name', r.name
                         )
                      ) as employee,
---                (
---                    SELECT json_build_object(
---                                   'id', e.id,
---                                   'full_name', e.full_name
---                           )
---                    FROM employees e
---                    WHERE e.id = ea.reviewer_employee_id
---                           LIMIT 1
---             ) AS reviewer,
                (
                    SELECT json_build_object(
                                   'id', p.id,
@@ -59,6 +50,169 @@ router.get('/list', checkAuth, userPermission, async (req, res) => {
         success: true,
         message: 'Activity fetched successfully',
         data: rows
+    })
+})
+
+router.get('/list/checkin', checkAuth, userPermission, async (req, res) => {
+    const { page, limit} = req.query;
+
+    let limits = '';
+    const offset = (page - 1) * limit < 0 ? 0 : (page - 1) * limit;
+
+    if (page && limit) {
+        limits = ` LIMIT ${limit} OFFSET ${offset} `;
+    }
+
+    const {rows} = await db.query(`
+        SELECT ea.*,
+               COUNT(*) OVER() AS total_count, json_build_object(
+                'id', e.id,
+                'full_name', e.full_name,
+                'role', json_build_object(
+                        'name', r.name
+                        )
+                     ) as employee,
+               (
+                   SELECT json_build_object(
+                                  'name', p.name
+                          )
+                   FROM project_members pm
+                   LEFT JOIN projects p ON p.id = pm.project_id
+                   WHERE e.id = pm.employee_id AND pm.status = 1
+                          LIMIT 1
+            ) AS project
+        FROM employee_activities ea
+            LEFT JOIN employees e ON e.id = ea.employee_id
+            LEFT JOIN employee_roles er ON e.id = er.employee_id
+            LEFT JOIN roles r ON r.id = er.role
+        WHERE EXISTS (
+            SELECT 1
+            FROM project_members pm1
+            JOIN project_members pm2 ON pm1.project_id = pm2.project_id AND pm2.status = 1
+            WHERE pm1.employee_id = ea.employee_id
+          AND pm2.employee_id = $1 AND pm1.status = 1 AND pm2.status = 1
+            )
+            AND ea.type = 3 AND ea.status = 1
+        ORDER BY e.full_name ASC ${limits ? limits : ''};
+    `, [req.currentUserId])
+
+    res.status(200).json({
+        success: true,
+        message: 'Activity fetched successfully',
+        data: {
+            total: rows?.[0]?.total_count || 0,
+            page: page,
+            data: rows
+        }
+    })
+})
+
+router.get('/list/checkout', checkAuth, userPermission, async (req, res) => {
+    const {page, limit} = req.query;
+
+    let limits = '';
+    const offset = (page - 1) * limit < 0 ? 0 : (page - 1) * limit;
+
+    if (page && limit) {
+        limits = ` LIMIT ${limit} OFFSET ${offset} `;
+    }
+
+    const {rows} = await db.query(`
+        SELECT ea.*,
+               COUNT(*) OVER() AS total_count, json_build_object(
+                'id', e.id,
+                'full_name', e.full_name,
+                'role', json_build_object(
+                        'name', r.name
+                        )
+                     ) as employee,
+               (
+                   SELECT json_build_object(
+                                  'name', p.name
+                          )
+                   FROM project_members pm
+                   LEFT JOIN projects p ON p.id = pm.project_id
+                   WHERE e.id = pm.employee_id AND pm.status = 1
+                          LIMIT 1
+            ) AS project
+        FROM employee_activities ea
+            LEFT JOIN employees e ON e.id = ea.employee_id
+            LEFT JOIN employee_roles er ON e.id = er.employee_id
+            LEFT JOIN roles r ON r.id = er.role
+        WHERE EXISTS (
+            SELECT 1
+            FROM project_members pm1
+            JOIN project_members pm2 ON pm1.project_id = pm2.project_id AND pm2.status = 1
+            WHERE pm1.employee_id = ea.employee_id  AND pm1.status = 1 AND pm2.status = 1
+          AND pm2.employee_id = $1
+            )
+            AND ea.type = 4 AND ea.status = 1 AND ea.completed_status = 0
+        ORDER BY e.full_name ASC ${limits ? limits : ''};
+    `, [req.currentUserId])
+
+    res.status(200).json({
+        success: true,
+        message: 'Activity fetched successfully',
+        data: {
+            total: rows?.[0]?.total_count || 0,
+            page: page,
+            data: rows
+        }
+    })
+})
+
+router.get('/list/atwork', checkAuth, userPermission, async (req, res) => {
+    const {page, limit} = req.query;
+
+    let limits = '';
+    const offset = (page - 1) * limit < 0 ? 0 : (page - 1) * limit;
+
+    if (page && limit) {
+        limits = ` LIMIT ${limit} OFFSET ${offset} `;
+    }
+
+    const {rows} = await db.query(`
+        SELECT ea.*,
+               COUNT(*) OVER() AS total_count, json_build_object(
+                'id', e.id,
+                'full_name', e.full_name,
+                'role', json_build_object(
+                        'name', r.name
+                        )
+                     ) as employee,
+               (
+                   SELECT json_build_object(
+                                  'name', p.name
+                          )
+                   FROM project_members pm
+                   LEFT JOIN projects p ON p.id = pm.project_id
+                   WHERE e.id = pm.employee_id AND pm.status = 1
+                          LIMIT 1
+            ) AS project
+        FROM employee_activities ea
+            LEFT JOIN employees e ON e.id = ea.employee_id
+            LEFT JOIN employee_roles er ON e.id = er.employee_id
+            LEFT JOIN roles r ON r.id = er.role
+        WHERE EXISTS (
+            SELECT 1
+            FROM project_members pm1
+            JOIN project_members pm2 ON pm1.project_id = pm2.project_id AND pm2.status = 1
+            WHERE pm1.employee_id = ea.employee_id  AND pm1.status = 1 AND pm2.status = 1
+          AND pm2.employee_id = $1
+            )
+            AND ea.type = 3 AND ea.status = 2 AND ea.completed_status = 0
+            
+        ORDER BY e.full_name ASC ${limits ? limits : ''};
+    `, [req.currentUserId])
+
+    res.status(200).json({
+        success: true,
+        message: 'Activity fetched successfully',
+        data: {
+            total: rows?.[0]?.total_count || 0,
+            page: page,
+            data: rows
+        }
     })
 })
 
