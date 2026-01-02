5b00f6eea3690d8b09f1127bb4d0c81168960245
alimahmudlu <alimahmudlu@gmail.com>
2026-01-03 02:29:52 +0400

    update

--- src/routes/admin/food.js
+++ src/routes/admin/food.js
@@ -373,6 +373,8 @@ router.get('/projects/:id', checkAuth, userPermission, async (req, res) => {
             p.id;
     `
 
+    console.log(query, [moment().format('YYYY-MM-DD'), moment().format('YYYY-MM-DD'), req.params.id]);
+
     const {rows: employees} = await db.query(query, [moment().format('YYYY-MM-DD'), moment().format('YYYY-MM-DD'), req.params.id]);
 
     return res.status(200).json({
