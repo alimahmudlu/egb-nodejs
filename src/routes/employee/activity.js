d3e1545adf18f380cf57e7b5db0f4d3da2a79a62
alimahmudlu <alimahmudlu@gmail.com>
2026-01-06 02:28:32 +0400

    update

--- src/routes/employee/activity.js
+++ src/routes/employee/activity.js
@@ -16,6 +16,8 @@ router.post('/checkin', checkAuth, userPermission, async (req, res) => {
     const {rows: empData} = await db.query(`SELECT full_name FROM employees WHERE id = $1`, [req.currentUserId]);
     const turn = moment(time).isBetween(moment("03:00", "HH:mm"), moment("17:00", "HH:mm")) ? 1 : 2;
 
+    console.log(turn, time, moment("03:00", "HH:mm"), moment("17:00", "HH:mm"))
+
     const {rows: checkedInRows} =
         await db.query(`
             SELECT * FROM employee_activities
