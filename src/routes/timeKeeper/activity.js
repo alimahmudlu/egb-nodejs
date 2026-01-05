5d8f84d6ae21eb19ce8c7298d1fe790e0eb30af4
alimahmudlu <alimahmudlu@gmail.com>
2026-01-06 02:42:39 +0400

    update

--- src/routes/timeKeeper/activity.js
+++ src/routes/timeKeeper/activity.js
@@ -748,10 +748,10 @@ router.post('/accept', checkAuth, userPermission, async (req, res) => {
     if (type === 2) {
         const {rows: checkInRow} = await db.query(`
         UPDATE employee_activities ea
-        SET completed_status = $1, work_time = $6, turn = $7
+        SET completed_status = $1, work_time = $6
         WHERE employee_id = $2 and status = $3 and completed_status = $4 and type = $5
             RETURNING *;
-    `, [1, employee_id, 2, 0, 1, `${diff?.hours}:${diff?.minutes}`, turn])
+    `, [1, employee_id, 2, 0, 1, `${diff?.hours}:${diff?.minutes}`])
     }
 
     const {rows} = await db.query(`
