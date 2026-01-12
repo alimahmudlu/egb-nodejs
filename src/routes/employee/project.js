0eace97ba50583d018d62ac6c8fb49a3244f659e
alimahmudlu <alimahmudlu@gmail.com>
2026-01-12 09:08:29 +0400

    update

--- src/routes/employee/project.js
+++ src/routes/employee/project.js
@@ -213,7 +213,7 @@ router.post('/item/:id/tasks/item/:task_id/status', checkAuth, userPermission, a
 
     if (files?.length > 0) {
         const valuesClause = files.map(
-            (row, i) => `($${i * 4 + 1}, $${i * 4 + 2}, $${i * 4 + 3}, $${i * 4 + 4}, $${i * 4 + 5})`
+            (row, i) => `($${i * 5 + 1}, $${i * 5 + 2}, $${i * 5 + 3}, $${i * 5 + 4}, $${i * 5 + 5})`
         ).join(', ');
 
         const values = files.map((row, i) => (
