0342316b42875c630e8beaf5a1ad9399a7f6e037
alimahmudlu <alimahmudlu@gmail.com>
2026-01-15 09:34:35 +0400

    update

--- src/routes/chief/reports.js
+++ src/routes/chief/reports.js
@@ -70,6 +70,117 @@ router.get('/list', checkAuth, userPermission, async (req, res) => {
     })
 })
 
+router.get('/list/item', checkAuth, userPermission, async (req, res) => {
+    const {name, employee_id, position_id, role_id, staff_status, checkin_status, project, dontShowSubcontractors, showSubcontractors, status, turn, checkType, start_date, end_date, today, id} = req.query
+    const filters = [];
+
+    if (name) {
+        filters.push(
+            `(LOWER(e.full_name) LIKE LOWER(%${name}%) OR LOWER(e.full_name_russian) LIKE LOWER(%${name}%))`
+        );
+    }
+    if (employee_id) {
+        filters.push(`e.id = ${employee_id}`);
+    }
+    if (position_id) {
+        filters.push(`e.position = ${position_id}`);
+    }
+    if (role_id) {
+        filters.push(`pm.role_id = ${role_id}`);
+    }
+    if (staff_status) {
+        filters.push(`ps.status = ${staff_status}`);
+    }
+    if (checkin_status === '1') {
+        filters.push(`checkin_status.employee_id IS NOT NULL`);
+    }
+    if (checkin_status === '2') {
+        filters.push(`checkin_status.employee_id IS NULL`);
+    }
+    if (project && project.length > 0) {
+        filters.push(`p.id IN (${project.join(',')})`);
+    }
+    if (dontShowSubcontractors) {
+        filters.push(`a.subcontract = false`);
+    }
+    if (showSubcontractors) {
+        filters.push(`a.subcontract = true`);
+    }
+
+    const whereClause = filters.length ? ` AND ${filters.join(' AND ')}` : '';
+
+
+    const query = `SELECT
+                       e.id AS employee_id,
+                       e.full_name,
+                       e.full_name_russian,
+                       ps.name AS employee_position_name,
+                       ps.name_ru AS employee_position_name_russian,
+                       r.name AS employee_role_name,
+                       STRING_AGG(p.name, ', ') AS employee_projects,
+                       ps.status AS employee_staff_status,
+                       CASE
+                           WHEN checkin_status.employee_id IS NOT NULL THEN TRUE
+                           ELSE FALSE
+                           END AS is_checked_in_on_date,
+                       checkin_status.turn AS employee_turn
+                   FROM employees e
+                            JOIN project_members pm
+                                 ON pm.employee_id = e.id AND pm.status = 1
+                            JOIN projects p
+                                 ON p.id = pm.project_id
+                            JOIN applications a
+                                 ON a.id = e.application_id
+                            LEFT JOIN positions ps
+                                      ON ps.id = e.position
+                            LEFT JOIN roles r
+                                      ON r.id = pm.role_id
+                            LEFT JOIN (
+                       SELECT
+                           ea.employee_id,
+                           MAX(ea.turn) AS turn
+                       FROM employee_activities ea
+                       WHERE
+                           ((ea.type = 1 OR ea.type = 3) AND ea.status = 2)
+
+                           ${status ? ` AND ea.is_manual = ${status === '1' ? true : false}` : ''}
+                           ${checkType ? ` AND ea.type = ${checkType}` : ''}
+                           ${turn ? ` AND ea.turn = ${turn}` : ''}
+                           ${start_date && end_date ?
+                                   `
+                            AND DATE(ea.review_time) >= '${start_date}'
+                            AND DATE(ea.review_time) <= '${end_date}'
+                            ` : ` AND DATE(ea.review_time) = '${today}'`}
+
+
+                       GROUP BY ea.employee_id
+                   ) AS checkin_status
+                                      ON checkin_status.employee_id = e.id
+                   WHERE ${id ? `p.id IN (${id})` : '1=1'} ${whereClause} AND e.is_active = TRUE
+                         ${end_date ?
+                                 ` AND DATE(a.employees_non_official_start_date) <= '${end_date}'`
+                                 :
+                                 ` AND DATE(a.employees_non_official_start_date) <= '${today}'`}
+                   GROUP BY
+                       e.id,
+                       e.full_name,
+                       e.full_name_russian,
+                       ps.name,
+                       ps.name_ru,
+                       r.name,
+                       ps.status,
+                       checkin_status.employee_id,
+                       checkin_status.turn
+                   ORDER BY e.id;`
+    const {rows} = await db.query(query, [])
+
+    res.json({
+        success: true,
+        message: 'Projects fetched successfully',
+        data: rows
+    })
+})
+
 router.get('/statistics', checkAuth, userPermission, async (req, res) => {
     const {project, start_date, end_date} = req.query
 
