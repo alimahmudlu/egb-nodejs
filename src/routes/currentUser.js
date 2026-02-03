42742dee5b58989984612d756b96449228358692
alimahmudlu <alimahmudlu@gmail.com>
2026-02-03 10:28:31 +0400

    update

--- src/routes/currentUser.js
+++ src/routes/currentUser.js
@@ -81,7 +81,10 @@ router.get('/rating', checkAuth, async (req, res) => {
     const {rows: userDataRows} =
         await db.query(`
                 SELECT
-                    (SELECT sum(t.points), sum(t.finalPoints) FROM tasks t WHERE (assigned_employee_id = e.id OR reporter_employee_id = e.id) AND EXISTS (
+                    (SELECT jsonb_build_object(
+                                    'points', sum(t.points),
+                                    'finalPoints', sum(t.finalPoints)
+                            ), FROM tasks t WHERE (assigned_employee_id = e.id OR reporter_employee_id = e.id) AND EXISTS (
                 SELECT 1
                 FROM task_activities ta
                 WHERE ta.task_id = t.id
