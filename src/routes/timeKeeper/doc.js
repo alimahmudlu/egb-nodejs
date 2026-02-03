3e67ffecbab160908b032762ae7f8d5db0358db3
alimahmudlu <alimahmudlu@gmail.com>
2026-02-03 19:59:44 +0400

    update

--- src/routes/timeKeeper/doc.js
+++ src/routes/timeKeeper/doc.js
@@ -19,31 +19,31 @@ router.get('/list', checkAuth, userPermission, async (req, res) => {
                                        WHERE au2.application_id = au.application_id
                                          AND au2.type = au.type
                                          AND au2.id > au.id
-                                   ) 
-                                     ${status === '2' ?
-        `au.date_of_expiry IS NOT NULL
+                                   )
+                                       ${status === '2' ?
+                                               `au.date_of_expiry IS NOT NULL
             AND (
                 (au.type = 'registration_card' AND au.date_of_expiry <= CURRENT_DATE + INTERVAL '7 days')
                 OR (au.type = 'metro_card' AND au.date_of_expiry <= CURRENT_DATE + INTERVAL '5 days')
                 OR (au.type != 'registration_card' AND au.date_of_expiry <= CURRENT_DATE + INTERVAL '30 days')
             )
             AND au.date_of_expiry > CURRENT_DATE`
-        : ''
-    }
-                                     ${status === '3' ?
-        `au.date_of_expiry IS NOT NULL AND au.date_of_expiry < CURRENT_DATE`
-        : ''
-    } 
-                                     ${status === '1' ?
-        `au.date_of_expiry IS NOT NULL
+                                               : ''
+                                       }
+                                       ${status === '3' ?
+                                               `au.date_of_expiry IS NOT NULL AND au.date_of_expiry < CURRENT_DATE`
+                                               : ''
+                                       }
+                                       ${status === '1' ?
+                                               `au.date_of_expiry IS NOT NULL
             AND au.type != 'migration_card'
             AND (
                 (au.type = 'registration_card' AND au.date_of_expiry > CURRENT_DATE + INTERVAL '7 days')
                 OR (au.type = 'metro_card' AND au.date_of_expiry <= CURRENT_DATE + INTERVAL '5 days')
                 OR (au.type != 'registration_card' AND au.date_of_expiry > CURRENT_DATE + INTERVAL '30 days')
             )`
-        : ''
-    }                              
+                                               : ''
+                                       }
                                      AND au.deleted_at IS NULL AND au.status = 1 AND NOT (
                                        a.country_id = 219 AND au.type = 'contract'
                                        );
