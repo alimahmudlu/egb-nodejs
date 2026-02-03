18a31eb32b6c620be35b6184a15caac963b324b6
alimahmudlu <alimahmudlu@gmail.com>
2026-02-03 10:29:41 +0400

    update

--- src/routes/currentUser.js
+++ src/routes/currentUser.js
@@ -84,7 +84,7 @@ router.get('/rating', checkAuth, async (req, res) => {
                     (SELECT jsonb_build_object(
                                     'points', sum(t.points),
                                     'finalPoints', sum(t.finalPoints)
-                            ), FROM tasks t WHERE (assigned_employee_id = e.id OR reporter_employee_id = e.id) AND EXISTS (
+                            ) FROM tasks t WHERE (assigned_employee_id = e.id OR reporter_employee_id = e.id) AND EXISTS (
                 SELECT 1
                 FROM task_activities ta
                 WHERE ta.task_id = t.id
