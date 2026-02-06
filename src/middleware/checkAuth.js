e05592efc4943c76c589b2ba1814d84521c82e5e
alimahmudlu <alimahmudlu@gmail.com>
2026-02-06 17:06:59 +0400

    update

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -5,25 +5,25 @@ import moment from "moment";
 async function checkAuth (req, res, next) {
     const access_token = req.headers?.authorization;
 
-    try {
-        const {id} = verifyJWT(access_token);
-        const {rows: userAuthRows} = await db.query('SELECT * FROM employee_auth WHERE employee_id = $1', [id])
-        const {rows: userRole} = await db.query('SELECT * FROM employee_roles WHERE employee_id = $1', [id])
-
-        if (!userAuthRows || userAuthRows.length === 0) {
-            throw new Error('User not found');
-        }
-        req.currentUserId = id;
-
-        next()
-    }
-    catch (error) {
+    // try {
+    //     const {id} = verifyJWT(access_token);
+    //     const {rows: userAuthRows} = await db.query('SELECT * FROM employee_auth WHERE employee_id = $1', [id])
+    //     const {rows: userRole} = await db.query('SELECT * FROM employee_roles WHERE employee_id = $1', [id])
+    //
+    //     if (!userAuthRows || userAuthRows.length === 0) {
+    //         throw new Error('User not found');
+    //     }
+    //     req.currentUserId = id;
+    //
+    //     next()
+    // }
+    // catch (error) {
         return res.status(401).json({
             message: 'Unauthorized',
-            error: error.message,
+            error: 'error.message',
             data: null
         });
-    }
+    // }
 }
 
 export default checkAuth
